a8daac4a1e2b99865e3bced10472ddd904d8ea76
Eric Tillberg <eric.tillberg@gmail.com>
2015-09-27 19:23:50 -0400

    refactored data_analysis.rb code

--- data_analysis.rb
+++ data_analysis.rb
@@ -2,28 +2,26 @@ require 'sqlite3'
 require 'gruff'
 
 @db = SQLite3::Database.new "collection.sqlite3"
-
-all_entries = @db.execute "SELECT * FROM trump_clinton_collection"
-
 @valid_entries = @db.execute "SELECT * FROM trump_clinton_collection WHERE (mood = \"happy\") OR (mood = \"sad\") OR (mood = \"disgusted\") OR (mood = \"surprised\") OR (mood = \"neutral\") OR (mood = \"scared\") OR (mood = \"angry\")"
 
+@politicians = {"clinton" => ["Clinton", "Hillary", "@HillaryClinton"], "trump" => ["Trump", "@realDonaldTrump"]}
+@twitter_feeds = {"breitbart" => "Breitbart News", "cbs" => "CBS Politics", "cnn" => "CNN Politics", "huffpo" => "Huffington Post Politics", "politico" => "Politico"}
+
 def analyze_moods(politician, twitter_feed)
-  if twitter_feed == "breitbart"
-    formal_feed_name = "Breitbart News"
-  elsif twitter_feed == "cbs"
-    formal_feed_name = "CBS Politics"
-  elsif twitter_feed == "cnn"
-    formal_feed_name = "CNN Politics"
-  elsif twitter_feed == "huffpo"
-    formal_feed_name = "Huffington Post Politics"
-  elsif twitter_feed == "politico"
-    formal_feed_name = "Politico"
+  
+
+  formal_feed_name = ""
+  @twitter_feeds.each do |feed|
+    if twitter_feed == feed[0]
+      formal_feed_name = feed[1]
+    end
   end
 
-  if politician == "clinton"
-    politician_names = ["Clinton", "Hillary", "@HillaryClinton"] 
-  elsif politician == "trump"
-    politician_names = ["Trump", "@realDonaldTrump"]
+  politician_names = []
+  @politicians.each do |pol|
+    if politician == pol[0]
+      politician_names = pol[1]
+    end
   end
 
   moods = @valid_entries.select { |entry| entry[1] == formal_feed_name && politician_names.include?(entry[3]) }
@@ -33,15 +31,12 @@ def analyze_moods(politician, twitter_feed)
   #dominant_mood = freq.max_by{ |k, v| v }[0]
 end
 
-politicians = ["clinton", "trump"]
-twitter_feeds = ["breitbart", "cbs", "cnn", "huffpo", "politico"]
-
-politicians.each do |politician|
-  twitter_feeds.each do |feed|
-    moods = analyze_moods("#{politician}", "#{feed}")
+@politicians.each do |politician|
+  @twitter_feeds.each do |feed|
+    moods = analyze_moods("#{politician[0]}", "#{feed[0]}")
     g = Gruff::Pie.new
     g.font = "/Library/Fonts/Arial.ttf"
-    g.title = "#{feed}'s representation of #{politician}"
+    g.title = "#{feed[0]}'s representation of #{politician[0]}"
     g.data 'Scared', moods['scared']
     g.data 'Happy', moods['happy']
     g.data 'Angry', moods['angry']
