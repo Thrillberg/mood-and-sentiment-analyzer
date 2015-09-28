a2a55e2cb8c00161ef310a9e2366208cce0258e0
Eric Tillberg <eric.tillberg@gmail.com>
2015-09-28 09:54:45 -0400

    just juggled some code around. No major changes (as far as I remember)

--- mood_and_sentiment_analysis.rb
+++ mood_and_sentiment_analysis.rb
@@ -1,6 +1,5 @@
 require 'sqlite3'
 require 'gruff'
-#require 'rmagick'
 
 @db = SQLite3::Database.new "collection.sqlite3"
 @valid_entries = @db.execute "SELECT * FROM trump_clinton_collection WHERE (mood = \"happy\") OR (mood = \"sad\") OR (mood = \"disgusted\") OR (mood = \"surprised\") OR (mood = \"neutral\") OR (mood = \"scared\") OR (mood = \"angry\")"
@@ -25,16 +24,34 @@ def analyze_moods(politician, twitter_feed)
 
   moods = @valid_entries.select { |entry| entry[1] == formal_feed_name && politician_names.include?(entry[3]) }
   freq = moods.inject(Hash.new(0)) { |h, v| h[v[6]] += 1;h }
-
-  #dominant_mood = freq.max_by{ |k, v| v }[0]
 end
 
+def analyze_sentiments(politician, twitter_feed)
+  formal_feed_name = ""
+  @twitter_feeds.each do |feed|
+    if twitter_feed == feed[0]
+      formal_feed_name = feed[1]
+    end
+  end
+
+  politician_names = []
+  @politicians.each do |pol|
+    if politician == pol[0]
+      politician_names = pol[1]
+    end
+  end
+
+  sentiments = @valid_entries.select { |entry| entry[1] == formal_feed_name && politician_names.include?(entry[3]) }
+  freq = sentiments.inject(Hash.new(0)) { |h, v| h[v[7]] += 1;h }
+end
+
+#mood charting
 @politicians.each do |politician|
   @twitter_feeds.each do |feed|
     moods = analyze_moods("#{politician[0]}", "#{feed[0]}")
     g = Gruff::Pie.new
     g.font = "/Library/Fonts/Arial.ttf"
-    g.title = "#{feed[0]}'s representation of #{politician[0]}"
+    g.title = "#{feed[0]}'s representation of #{politician[0]}'s mood"
     g.data 'Scared', moods['scared']
     g.data 'Happy', moods['happy']
     g.data 'Angry', moods['angry']
@@ -47,6 +64,23 @@ end
     blend = Magick::Image::read("#{feed[0]}.png").first.resize(100, 50)
     img = img.blend(blend, '100%', '100%', 50, 125)
     img.write("#{politician[0]}_#{feed[0]}_with_overlay.png")
-    require 'pry';binding.pry
+  end
+end
+
+#sentiment charting
+@politicians.each do |politician|
+  @twitter_feeds.each do |feed|
+    sentiments = analyze_sentiments("#{politician[0]}", "#{feed[0]}")
+    g = Gruff::Pie.new
+    g.font = "/Library/Fonts/Arial.ttf"
+    g.title = "sentiment associated with #{politician[0]} in #{feed[0]}"
+    g.data 'Negative', sentiments['negative']
+    g.data 'Neutral', sentiments['neutral']
+    g.data 'Positive', sentiments['positive']
+    g.write("#{politician[0]}_#{feed[0]}_sentiment.png")
+    img = Magick::Image::read("#{politician[0]}_#{feed[0]}_sentiment.png").first
+    blend = Magick::Image::read("#{feed[0]}.png").first.resize(100, 50)
+    img = img.blend(blend, '100%', '100%', 50, 125)
+    img.write("#{politician[0]}_#{feed[0]}_sentiment.png")
   end
 end
